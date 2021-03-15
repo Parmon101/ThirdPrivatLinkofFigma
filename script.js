48e1e6acc69c33e35957e56229054972d95f5b59
Evgeny <48516892+Parmon101@users.noreply.github.com>
2021-03-15 22:17:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,25 +3,27 @@
 
 // настройка переключения странице в левом меню
 let sideButtons = document.querySelectorAll('[data-sidebar]')
-let pages = document.querySelectorAll('[data-page]')
 
 
 for (let sideButton of sideButtons) {
-    sideButton.addEventListener('click', function(e) {
+    sideButton.addEventListener('click', function(event) {
 
         // при клике, все страницы скрываются
-        pages.forEach(element => {
-        element.classList.add('hidden')});
+        document.querySelectorAll('[data-page]').forEach(element => {
+            element.classList.add('hidden')
+        });
 
         // получаем имя кнопки сайдбара
-        let field = this.getAttribute('data-sidebar');
+        let field = event.currentTarget.getAttribute('data-sidebar');
 
         // ищем нужную страницу по имению (взятого из field) кнопки сайдбара
-        let page = document.querySelector(`[data-page=${field}]`)
+        let page = document.querySelector(`[data-page=${field}]`);
 
         // проверяем, если скрыта\hidden и добавляем показ\show
-        if (page.classList.contains('hidden'))
-        {page.classList.remove('hidden') ||page.classList.add('show') }
+        if (page) {
+            page.classList.remove('hidden');
+            page.classList.add('show')
+        } 
 
     })
 }
@@ -37,25 +39,93 @@ for (let card of cards) {
     card.addEventListener('click', upClass) 
 }
 
+// function upClass(event){
+//     const selectCard=event.currentTarget;
+    
+//     cards.forEach(element => {
+//         element.classList.remove('activeCard')
+//     });
+
+
+//     if (!selectCard.classList.contains('activeCard'))
+//     {selectCard.classList.add('activeCard')}
+
+// }
+
 function upClass(event){
+    const selectCard=event.currentTarget;
     
     cards.forEach(element => {
         element.classList.remove('activeCard')
     });
 
-    event=event.currentTarget
+    if (!selectCard.classList.contains('activeCard'))
+    {selectCard.classList.add('activeCard')}
 
-    if (!event.classList.contains('activeCard'))
-    {event.classList.add('activeCard')}
+    let block = selectCard.getAttribute('data-id-card');
+
+    document.querySelectorAll('[data-value]').forEach(element => {
+
+
+        let name = element.getAttribute('data-value');
+        let value = element.textContent
+
+
+        // value.innerHTML = `${name}[${value}]`
+
+        // value.innerHTML = Overdue[AvFirstRes]
+
+        // value.innerHTML =  12412;
+
+        value.innerHTML = Open.Tasks
+
+        console.log(value);
+
+
+    //    console.log(`block ${block} : name ${name} =  ${value}`);
+    
+});
+
+// let fifi = document.querySelector('[data-value]')
+// value.innerHTML = `${block}.${name}`
+
+
+// let fifi = document.querySelector('[data-value]')
+// fifi.innerHTML = Open.Tasks
 
 }
 
 
 
+let Unresolved = {
+    Tasks: 500,
+    Clients: 250,
+    AvFirstRes: '48m',
+    AvResTime: '21h 15m',
+    ResolutionWithSLA:  '50%',
+    }
 
 
-// view detaiks и view all
-// let asd = document.querySelector('.asd')
-// asd.addEventListener('click', function(){
-//     console.log(`${cards}`);
-// })
+let Overdue = {
+    Tasks: 449,
+    Clients: 426,
+    AvFirstRes: '33m',
+    AvResTime: '3h 8m',
+    ResolutionWithSLA:  '94%',
+    }
+
+let Open = {
+    Tasks: 300,
+    Clients: 257,
+    AvFirstRes: '1d 5h 18m',
+    AvResTime: '2d 10h 5m',
+    ResolutionWithSLA:  '58%',
+    }
+
+let OnHold = {
+    Tasks: 0,
+    Clients: 0,
+    AvFirstRes: 'Unknown',
+    AvResTime: 'Unknown',
+    ResolutionWithSLA:  '0%',
+    }
